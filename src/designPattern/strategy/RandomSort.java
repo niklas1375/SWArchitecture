a597ee41ff41f86a4207b7b7344ff0693648cb7f
Niklas <s131454@student.dhbw-mannheim.de>
2016-03-07 12:54:41 +0100

    Random Sort optimiert auf 1/4 der Zeit

--- src/designPattern/strategy/RandomSort.java
+++ src/designPattern/strategy/RandomSort.java
@@ -3,29 +3,40 @@ package designPattern.strategy;
 import java.util.Arrays;
 
 public class RandomSort implements SorterIF {
+	int[] sorted;
 
 	@Override
 	public int[] sort(int[] sourceArray) {
+		sorted = Arrays.copyOf(sourceArray, sourceArray.length);
+		Arrays.sort(sorted);
+		long time = System.currentTimeMillis();
 		int[] targetArray = Arrays.copyOf(sourceArray, sourceArray.length);
+		int i = 0;
 		while (!isSorted(targetArray)) {
 			shuffle(targetArray);
+			if (i%10000000 == 0 && i != 0) {
+				System.out.println("Zwischenstand " + i + " in " + (System.currentTimeMillis() - time) / 1000.0);
+			}
+			i++;
 		}
 		return targetArray;
 	}
 	
 	private void shuffle(int[] i) {
-		for (int x = 0; x < i.length; ++x) {
+		for (int x = 0; x < i.length; x++) {
 			int index1 = (int) (Math.random() * i.length);
-			int index2 = (int) (Math.random() * i.length);
-			int a = i[index1];
-			i[index1] = i[index2];
-			i[index2] = a;
+			int a = i[x];
+			i[x] = i[index1];
+			i[index1] = a;
 		}
 	}
 	
-	private boolean isSorted(int[] i){
-		for (int x = 0; x < i.length - 1; ++x) {
-			if (i[x] > i[x + 1]) {
+	private boolean isSorted(int[] toCheck){
+		if (toCheck.length == 1) {
+			return true;
+		}
+		for (int i = 1;i < toCheck.length; i++) {
+			if (toCheck[i - 1] > toCheck[i]) {
 				return false;
 			}
 		}
